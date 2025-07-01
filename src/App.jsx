8c2f9a027cba0e8b75a1fa996dda63732f01a0dc
Abhi044738 <110322117+Abhi044738@users.noreply.github.com>
2025-07-01 20:08:07 +0530

    Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,22 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Home } from "./pages/Home";
 import { Project } from "./pages/Project";
-import homeIcon from "./assets/home.png";
-import projectIcon from "./assets/project.png";
-import aboutIcon from "./assets/about.png";
+import { Navbar } from "./Component/Navbar";
+import { useRef } from "react";
 
 function App() {
+  const pageRefference = useRef(null);
+
   return (
     <div className=" h-200 w-200 border-2 border-soild">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/project" element={<Project />} />
-      </Routes>
-      <div className="flex  justify-evenly items-center  fixed bottom-8 left-1/2 transform -translate-x-1/2 min-w-2xs min-h-16 border-2 border-solid rounded-4xl bg-white shadow-xl/25  shadow-white">
-        <img
-          className="block w-10 h-10 object-contain"
-          src={homeIcon}
-          alt="home"
-        />
-        <img
-          className="block w-10 h-10 object-contain"
-          src={projectIcon}
-          alt="home"
-        />
-        <img
-          className="block w-10 h-10 object-contain"
-          src={aboutIcon}
-          alt="home"
-        />
+      <div className="border-red-200  border-2" ref={pageRefference}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/project" element={<Project />} />
+        </Routes>
       </div>
+
+      <Navbar pageRefference={pageRefference} />
     </div>
   );
 }
---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,82 @@
+import "./style.css";
+import homeIcon from "../assets/home.png";
+import projectIcon from "../assets/project.png";
+import aboutIcon from "../assets/about.png";
+import { useState } from "react";
+import { motion, AnimatePresence } from "motion/react";
+import { useNavigate } from "react-router-dom";
+
+const icons = [
+  { name: "home", path: homeIcon },
+  { name: "project", path: projectIcon },
+  { name: "about", path: aboutIcon },
+];
+
+export const Navbar = ({ pageRefference }) => {
+  const navigate = useNavigate();
+  const handDragEnd = (value, event, info) => {
+    const { x, y } = info.point;
+    console.log(info);
+
+    const dropTarget = document.elementFromPoint(x, y);
+    if (pageRefference.current.contains(dropTarget)) {
+      switch (value) {
+        case "home":
+          navigate("/");
+          break;
+        case "project":
+          navigate("/project");
+          break;
+        case "about":
+          navigate("/");
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
+  const [hover, setHover] = useState(false);
+  const handleHover = () => {
+    setHover(true);
+    setTimeout(() => setHover(false), 2000);
+  };
+  return (
+    <div className="fixed bottom-8 m-auto p-auto transform -translate-x-1/2  left-1/2">
+      <div className="relative ">
+        <AnimatePresence>
+          {hover ? (
+            <motion.p
+              animate={{ scale: [0, 1], opacity: [0, 1] }}
+              exit={{ scale: 0, opacity: 0 }}
+              transition={{
+                delay: 0.3,
+                ease: "easeInOut",
+              }}
+            >
+              drag and drop the icon
+            </motion.p>
+          ) : null}
+        </AnimatePresence>
+      </div>
+      <div
+        onMouseEnter={handleHover}
+        className="flex  justify-evenly items-center    min-w-2xs min-h-16 border-2 border-solid rounded-4xl bg-white shadow-xl/25  shadow-white"
+      >
+        {icons.map(({ name, path }, index) => (
+          <motion.img
+            key={name}
+            drag
+            dragSnapToOrigin
+            whileDrag={{ scale: 0.8 }}
+            onDragEnd={(event, info) => handDragEnd(name, event, info)}
+            whileHover={{ y: [0, -5, 0] }}
+            className="block w-10 h-10 object-contain"
+            src={path}
+            alt={name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
